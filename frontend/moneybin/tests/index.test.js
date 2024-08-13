e9ca47efa45ad21372e1a540c76f821cb840d411
Harald Mack <harald.mack@protonmail.com>
2024-08-13 13:09:37 +0200

    add a dummy test to check husky

--- frontend/moneybin/tests/index.test.js
+++ frontend/moneybin/tests/index.test.js
@@ -1,7 +1,13 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
+	});
+});
+
+describe('dummy test', () => {
+	it('always passes', () => {
+		expect(true).toBeTruthy();
 	});
 });
